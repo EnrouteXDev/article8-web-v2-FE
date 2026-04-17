f4d90d45f19e373f34913d3f3524458ebd8a2a4a
EnrouteXDev <DevTeam@enroutex.co.uk>
2026-04-17 12:32:51 +0100

    Clean up conflicting redeploy comments
    Removed conflicting comments that triggered redeploys.

--- app/(marketing)/products/[id]/page.tsx
+++ app/(marketing)/products/[id]/page.tsx
@@ -10,12 +10,4 @@ export default function ProductDetailPage() {
     </main>
   );
 }
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
 // random comment to trigger redeploy
->>>>>>> 2050ca7090aa2d19b932366dc4b24f91f60b3798
-=======
-// random comment to trigger redeploy 2
->>>>>>> a80f1d6670085f4daabb258bd043ab09c34d962c
